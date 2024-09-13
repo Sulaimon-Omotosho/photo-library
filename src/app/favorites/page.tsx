f2087f1e9c7241e9a591d34ad9e7d98ecdc31b6c
Sulaimon Omotosho <susudef2u@gmail.com>
2024-09-13 12:19:55 +0100

    Feat: Adding Favorites

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,16 +1,11 @@
-import UploadButton from '@/components/UploadButton'
 import React from 'react'
 import cloudinary from 'cloudinary'
 import ImageViewer from '@/components/ImageViewer'
+import { SearchResults } from '../gallery/page'
 
-export type SearchResults = {
-  public_id: string
-  tags: string[]
-}
-
-const GalleryPage = async () => {
+const FavoritesPage = async () => {
   const results = (await cloudinary.v2.search
-    .expression('resource_type:image')
+    .expression('resource_type:image AND tags=favorite')
     .sort_by('created_at', 'desc')
     .with_field('tags')
     .max_results(10)
@@ -20,13 +15,12 @@ const GalleryPage = async () => {
     <section className=''>
       <div className='flex flex-col gap-8'>
         <div className='flex justify-between'>
-          <h1 className='text-4xl font-bold'>Gallery</h1>
-          <UploadButton />
+          <h1 className='text-4xl font-bold'>Favorites</h1>
         </div>
         <div className='grid grid-cols-4 gap-4'>
           {results.resources.map((result) => (
             <ImageViewer
-              path='/gallery'
+              path='/favorites'
               key={result.public_id}
               imageData={result}
               width='400'
@@ -40,4 +34,4 @@ const GalleryPage = async () => {
   )
 }
 
-export default GalleryPage
+export default FavoritesPage
--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -2,17 +2,36 @@
 
 import { CldImage } from 'next-cloudinary'
 import React from 'react'
+import { Heart } from './icons/Heart'
+import { setAsFavorite } from '@/lib/actions'
+import { SearchResults } from '@/app/gallery/page'
+import { FavHeart } from './icons/FavHeart'
 
-const ImageViewer = (props: any) => {
+const ImageViewer = (
+  props: any & { imageData: SearchResults; path: string }
+) => {
+  const { imageData } = props
+  const isFavorite = imageData.tags.includes('favorite')
+
   return (
-    <CldImage
-      // width={500}
-      // height={300}
-      {...props}
-      // src={publicId}
-      // sizes='100vw'
-      // alt='image'
-    />
+    <div className='relative'>
+      <CldImage {...props} src={imageData.public_id} />
+      {isFavorite ? (
+        <FavHeart
+          onClick={() => {
+            setAsFavorite(imageData.public_id, false, props.path)
+          }}
+          className='absolute top-2 right-2 bg-slate-400 p-2 rounded-full bg-opacity-50 cursor-pointer text-red-500'
+        />
+      ) : (
+        <Heart
+          onClick={() => {
+            setAsFavorite(imageData.public_id, true, props.path)
+          }}
+          className='absolute top-2 right-2 bg-slate-400 p-2 rounded-full bg-opacity-50 cursor-pointer hover:text-red-500'
+        />
+      )}
+    </div>
   )
 }
 
